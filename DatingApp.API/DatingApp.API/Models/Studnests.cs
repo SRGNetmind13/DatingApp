aba679d9f9e93c20cd8d6d8de8652eb3d5739af0
SRGNetmind13 <simbak.netmind@hotmail.com>
2020-03-29 22:30:35 -0600

    Se genero clase stuendes

---
+++ DatingApp.API/DatingApp.API/Models/Studnests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Models
+{
+    public class Studnests
+    {
+        public int Id { get; set; }
+        public string NameStudne { get; set; }
+    }
+}
