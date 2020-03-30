b169618747a503dd90334ab6196b34d6355096f2
SRGNetmind13 <simbak.netmind@hotmail.com>
2020-03-29 22:17:53 -0600

    Se genero sports

---
+++ DatingApp.API/DatingApp.API/Models/Sports.cs
@@ -0,0 +1,10 @@
+namespace DatingApp.API.Models
+{
+    public class Sports
+    {
+        public int Id { get; set; }
+        public string Playe { get; set; }
+        public string SportToPlay { get; set; }
+        public string Age { get; set; }
+    }
+}
